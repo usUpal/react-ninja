5e2be86dec1b39b0512b602857ef45ebd24585ae
usUpal <udyan.upal@northsouth.edu>
2021-05-18 12:02:13 +0600

    click events

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const clickHandler = (name) => {
+        console.log(`hello ${name}`)
+    }
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            <button>Click me</button>
+            <button onClick = {
+                () => clickHandler('upal') //! inmoke as a annonymous function
+                
+            }>Invoke my name</button> 
         </div>
      );
 }
