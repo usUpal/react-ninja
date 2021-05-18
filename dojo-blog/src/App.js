ed88b61501fa75030988a7ce08507787c3674ce1
usUpal <udyan.upal@northsouth.edu>
2021-05-18 10:58:42 +0600

    dynamic content in template

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const syfy = 'The Expanse'
+  const person = {name: "upal", age: 30}
+  const ggl = "https://www.google.com"
   return (
     <div className="App">
       <div className="content">
-        <h1>APP COMPONENT</h1>
+        <h1>{"Blog"}</h1>
+        <p>{`hi internet, my name is ${person.name}`}</p>
+        <p>{`I'm currently watching ${syfy}`}</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{`random numbers: ${Math.round(Math.random()*6)}`}</p>
+        <a href={ggl}>Google</a>
       </div>
     </div>
   );
