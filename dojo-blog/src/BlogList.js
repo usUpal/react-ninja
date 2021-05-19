2fca93672b8fd40ed8ea4c7641939b7ee605b206
usUpal <udyan.upal@northsouth.edu>
2021-05-19 07:44:51 +0600

    props 1.1

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,9 +1,9 @@
-const BlogList = (props) => {
-    const blogItems = props.blogItems
-    const title = props.titles
+const BlogList = ({blogItems, titles}) => {
+    // const blogItems = props.blogItems
+    // const title = props.titles
     return ( 
         <div className="blog-list">
-            <h2>{ title }</h2>
+            <h2>{ titles }</h2>
             {blogItems.map((blog)=>(
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
