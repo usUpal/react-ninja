617167ee40935a8efc241352ea854b2d12e6d307
usUpal <udyan.upal@northsouth.edu>
2021-05-19 08:09:55 +0600

    reusing components

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -5,16 +5,15 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         {title:'My name is Elon', body: 'lorem..', author:'elon', id: 1},
         {title:'My name is Steve', body: 'lorem..', author:'steve', id: 2},
-        {title:'My name is Bill', body: 'lorem..', author:'bill', id: 3}
+        {title:'My name is Bill', body: 'lorem..', author:'bill', id: 3},
+        {title:'My name is Musk', body: 'lorem..', author:'elon', id: 4}
     ]);
     return ( 
         <div className="home">
             {/* \<h2>All Blogs</h2> */}
             <BlogList blogItems = {blogs} titles= "All Blogs"/> 
+            <BlogList blogItems = {blogs.filter((blog) => blog.author === 'elon')} titles= "Elon's Blog"/> 
             
-           
-            
-
         </div>
      );
 }
