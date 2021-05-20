0697bf0f80a9d82c366201369cd99cd8ac02373a
usUpal <udyan.upal@northsouth.edu>
2021-05-20 13:35:56 +0600

    route Parameters

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -3,6 +3,7 @@ import NavBar from './NavBar'
 import Home from "./Home"
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   
@@ -17,6 +18,9 @@ function App() {
           </Route>
           <Route path = "/create">
               <Create />
+          </Route>
+          <Route path = "/blogs/:id">
+              <BlogDetails />  
           </Route>
         </Switch>
         
--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 const BlogList = ({blogItems, titles}) => {
     // const blogItems = props.blogItems
     // const title = props.titles
@@ -6,9 +7,11 @@ const BlogList = ({blogItems, titles}) => {
             <h2>{ titles }</h2>
             {blogItems.map((blog)=>(
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <h3>{blog.body}</h3>
-                    <p>Written by {blog.author}</p>
+                    <Link to = {`/blogs/${blog.id}`}>
+                        <h2>{blog.title}</h2>
+                        {/* <h3>{blog.body}</h3> */}
+                        <p>Written by {blog.author}</p>
+                    </Link>
                     {/* <button onClick = {()=>handleDelete(blog.id)}>Delete</button> */}
 
                 </div>
